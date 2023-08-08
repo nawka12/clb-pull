76f97b253ec4dee4f08e16275094c461b0f8e871
KayfaHaarukku <54880732+nawka12@users.noreply.github.com>
2023-08-08 15:40:36 +0700

    Update webui-user.sh
    arguments

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,7 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-#export COMMANDLINE_ARGS=""
+export COMMANDLINE_ARGS="--xformers --lowram --theme dark --no-half-vae --share --enable-insecure-extension-access"
 
 # python3 executable
 #python_cmd="python3"
